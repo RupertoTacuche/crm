e678de8e60cb397e4bdc6f604049e4dfedf9dbeb
@RupertoTacuche <murcio2490@gmail.com>
2024-02-09 11:15:48 -0600

    video_169

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -5,13 +5,21 @@ import Error from '../components/Error'
 export async function action({request}) {
   
   const formData = await request.formData()
+
   const datos = Object.fromEntries(formData)
+
+  const email = formData.get('email')
   //validacion
   const errores = []
   if(Object.values(datos).includes('')){
     errores.push("Todos los campos son obligatorios") 
   }
 
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)){
+    errores.push('El email no es valido')
+  }
+  
   // Retornar datos si hay errores
   if(Object.keys(errores).length){
       return errores
@@ -47,6 +55,7 @@ function NuevoCliente() {
 
           <Form
             method= 'post'
+            noValidate
             
           >
               <Formulario />
